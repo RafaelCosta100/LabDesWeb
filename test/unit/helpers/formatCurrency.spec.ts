8674c9212874632f3a1a18e1efdc1df548f4cb3e
RafaelCosta100 <rafa_sc@hotmail.com>
2026-03-09 20:43:36 -0300

    inclusão do formatCurrency.spec.ts

---
+++ test/unit/helpers/formatCurrency.spec.ts
@@ -0,0 +1,17 @@
+/** 
+ *  describe: suites de testes => agrupamentos
+ *  
+*/
+describe("", () =>{
+/**
+ * test: testes individuais
+ * teste de cada possivel cenário de sucesso ou erro
+ */
+    test("",()=>{
+
+});
+
+    test("",() =>{
+
+});
+});
