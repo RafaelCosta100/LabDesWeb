7a9f62f72fd296d43c943aeef7360d883df4d81a
RafaelCosta100 <rafa_sc@hotmail.com>
2026-03-09 20:31:00 -0300

    alterações no controler e interface

--- src/controller/productController.ts
+++ src/controller/productController.ts
@@ -1,10 +1,11 @@
+import { formatCurrency } from '../helpers/formatCurrency';
 import {IProduct} from '../interfaces/IProduct';
 
 export const listProduct = (): Promise<IProduct[]> =>{
   const product: IProduct = {
     name: 'Bolacha',
     brand: 'Trakinas',
-    price: 2.99,
+    price: formatCurrency(2.99),
   };
 
   return Promise.resolve([product]);
