15aeed1bdc9e746a97ac3fffd0a207991f96e83c
RafaelCosta100 <rafa_sc@hotmail.com>
2026-03-09 22:24:33 -0300

    inclusão de testes com mock

--- test/integration/productController.spec.ts
+++ test/integration/productController.spec.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { app } from '../../src/app';
+import *as productController from '../../src/controller/productController'
 describe("Product Controller",()=>{
     it("should return a product list, when all succeeds", async () => {
         //Arrange
@@ -9,5 +10,13 @@ describe("Product Controller",()=>{
         //Assert
         expect (response.status).toBe(200);
         expect (response.body).toEqual([{"brand": "Trakinas", "name": "Bolacha", "price": "R$2,99"}]);
+    });
+    it("should not return error when empty list is returned", async () => {
+        //Arrange
+        jest.spyOn(productController,"listProduct").mockResolvedValueOnce([]);
+        //Act
+        const response = await request (app).get('/product').send();
+        //Assert
+        expect(response.body).toEqual([]);
     });
 })
