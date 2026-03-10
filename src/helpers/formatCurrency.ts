589a028c8d9e76ff6d2f1e00702abd366803dbb8
RafaelCosta100 <rafa_sc@hotmail.com>
2026-03-09 21:32:51 -0300

    alterações no formatCurrency e formatCurrency.spec

--- src/helpers/formatCurrency.ts
+++ src/helpers/formatCurrency.ts
@@ -2,6 +2,6 @@ export const formatCurrency = (value: number): string => {
 const formattedCurrency = value.toLocaleString("pt-br",{
     style:"currency",
     currency:'BRL',
-});
+}).replace(/\u00A0/g,'');
 return formattedCurrency
 };
--- test/unit/helpers/formatCurrency.spec.ts
+++ test/unit/helpers/formatCurrency.spec.ts
@@ -1,16 +1,32 @@
+import { formatCurrency } from "../../../src/helpers/formatCurrency";
+
 /** 
  *  describe: suites de testes => agrupamentos
  *  
 */
-describe("", () =>{
+describe('formatCurrency', () =>{
+    beforeAll(() => {
+        //instruções a serem executadas antes de todos os testes da suite
+    })
+    afterAll(() =>{
+        //instruções a serem executadas após todos os testes da suite
+    })
+    beforeEach(()=>{
+        //instruções a serem executadas antes de cada teste da suite
+    })
+    //afterEach...
 /**
  * test / it: testes individuais
  * teste de cada possivel cenário de sucesso ou erro
  */
-    test("",()=>{});
-
-    test('format currency (5.31) => R$5,31',() =>{});
+   // test('format currency (5.31) => R$5,31',() =>{});
 
     it("should return R$5,31 when value = 5.31", () =>{});
-    // AAA - Arange
+    // AAA - Arange, Act, Assert
+    //Arange
+    const value = 5.31;
+    //Act
+    const formattedValue = formatCurrency(value);
+    //Assert
+    expect(formattedValue).toContain('5,31');
 });
